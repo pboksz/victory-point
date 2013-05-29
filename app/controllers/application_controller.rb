0941ed3a4bb45cb7f8e81707d66a254bfbf271b0
Phillip Boksz <phillip.boksz@llp.pl>
2013-05-29 17:11:25 +0200

    Add CSRF protection for angular requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
 
   protect_from_forgery
+
+  after_filter :set_csrf_cookie_for_angular
+
+  def set_csrf_cookie_for_angular
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+  protected
+
+  def verified_request?
+    super || form_authenticity_token == request.headers['X_XSRF_TOKEN']
+  end
 end
